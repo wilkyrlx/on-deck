1deaa66655d8d8848171e84c925c7c7726120172
wilkyrlx <john_wilkinson@brown.edu>
2022-12-02 02:58:12 -0500

    add: frontend react boilerplate and config

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
+
+
+function App() {
+
+	return (
+		<div className="app">
+			
+		</div>
+	);
+}
+
+export default App;
