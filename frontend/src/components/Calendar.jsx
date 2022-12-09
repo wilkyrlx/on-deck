875bb670417dfeda227a09c94fd9a3e9735d9b3a
wilkyrlx <john_wilkinson@brown.edu>
2022-12-08 19:56:34 -0500

    push to new branch

--- frontend/src/components/Calendar.jsx
+++ frontend/src/components/Calendar.jsx
@@ -12,8 +12,15 @@ const events = [
     image: 'test',
   },
   {
-    id: 1,
-    title: 'event 1',
+    id: 2,
+    title: 'event 2',
+    start: '2022-12-02T10:00:00',
+    end: '2022-12-02T12:00:00',
+    image: 'test 2',
+  },
+  {
+    id: 3,
+    title: 'event 3',
     start: '2022-12-02T10:00:00',
     end: '2022-12-02T12:00:00',
     image: 'test 2',
@@ -27,7 +34,7 @@ function FullCalendarApp() {
         plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
         initialView="timeGridDay"
         headerToolbar={{
-          center: 'dayGridMonth,timeGridWeek,timeGridDay new',
+          center: 'timeGridWeek,timeGridDay new',
         }}
         customButtons={{
           new: {
