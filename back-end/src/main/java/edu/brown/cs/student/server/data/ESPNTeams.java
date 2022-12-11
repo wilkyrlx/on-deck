e0fce9732af0901bcaf5b121c34b9506fd44e509
Adrien Chavarot <chavarota@gmail.com>
2022-12-10 19:40:15 -0500

    using slug

--- back-end/src/main/java/edu/brown/cs/student/server/data/ESPNTeams.java
+++ back-end/src/main/java/edu/brown/cs/student/server/data/ESPNTeams.java
@@ -6,7 +6,7 @@ public record ESPNTeams(List<Sport> sports) {
   public record Sport(List<League> leagues) {
     public record League(List<TeamWrapper> teams) {
       public record TeamWrapper(Team team) {
-        public record Team(String id, String displayName, String color, List<Logo> logos, List<Link> links) {
+        public record Team(String id, String slug, String color, List<Logo> logos, List<Link> links) {
           public record Logo(String href) {
 
           }
