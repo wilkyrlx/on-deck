8762425915f54ceb8d602f39afbbcf6ac7fcbd09
cm3cm <morancaleb3@gmail.com>
2022-12-14 20:42:13 -0500

    docs: javadocs TeamID

--- back-end/src/main/java/edu/brown/cs/student/server/data/TeamID.java
+++ back-end/src/main/java/edu/brown/cs/student/server/data/TeamID.java
@@ -10,26 +10,37 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class TeamID {
+/**
+ * A class to pull the ESPN team IDs from the API.
+ * Produces a Map from team "slug" to ID.
+ */
+public final class TeamID {
   private final Map<String, String> teamNameToIDs = new HashMap<>();
 
-  private TeamWrapper.Team extractTeam(TeamWrapper tw) {
-    return tw.team();
-  }
+  /**
+   * Modifies the teamNameToIDs field.
 
+   * @param response the deserialized JSON representing available teams
+   */
   private void constructHashMap(ESPNTeams response) {
     List<TeamWrapper> teamWrapperList = response.sports().get(0).leagues().get(0).teams();
     List<TeamWrapper.Team> teamList = new ArrayList<>();
 
     for (TeamWrapper teamWrapper : teamWrapperList) {
-      teamList.add(this.extractTeam(teamWrapper));
+      teamList.add(teamWrapper.team());
     }
-    for (TeamWrapper.Team team: teamList) {
+    for (TeamWrapper.Team team : teamList) {
       this.teamNameToIDs.put(team.slug(), team.id());
       /* slug is the team name in a computer-friendly format */
     }
   }
 
+
+  /**
+   * Constructor for the TeamID class.
+
+   * @param sportsHandler takes in a SportHandler for access to Moshi
+   */
   public TeamID(SportsHandler sportsHandler) {
     final String API_URL_STUB = "https://site.api.espn.com/apis/site/v2/sports/";
     final List<String> leagueList = Arrays.asList(
@@ -52,6 +63,12 @@ public class TeamID {
     }
   }
 
+  /**
+   * Returns the team ID of the given team.
+
+   * @param teamName the name of the sports team
+   * @return the team's ESPN internal ID
+   */
   public String getTeamID(String teamName) {
     return this.teamNameToIDs.get(teamName);
   }
