444a3e7af420ca0ea35da51a4e4f31624552a86d
cm3cm <morancaleb3@gmail.com>
2022-12-16 21:01:32 -0500

    test: starts testing team ID

--- back-end/src/test/java/edu/brown/cs/student/server/data/TeamIDTest.java
+++ back-end/src/test/java/edu/brown/cs/student/server/data/TeamIDTest.java
@@ -1,3 +1,25 @@
-public class TeamIDTest {
+package edu.brown.cs.student.server.data;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.squareup.moshi.Moshi;
+import edu.brown.cs.student.util.ServerFailureException;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TeamIDTest {
+  private static final Moshi moshi = new Moshi.Builder().build();
+  private TeamID teamID;
+
+  @Before
+  public void setUp() throws ServerFailureException {
+    teamID = new TeamID(moshi);
+  }
+
+  @Test
+  public void getTeamIDNBA() {
+    assertEquals(teamID.getTeamID("atlanta-hawks"), "1");
+    assertEquals(teamID.getTeamID("milwaukee-bucks"), "15");
+    assertEquals(teamID.getTeamID("charlotte-hornets"), "30");
+  }
 }
