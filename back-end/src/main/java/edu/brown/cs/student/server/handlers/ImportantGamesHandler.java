670649357f33fa171ca23dfacb3d3a3209b6690f
cm3cm <morancaleb3@gmail.com>
2022-12-16 20:45:04 -0500

    refactor: removes class field of Map in ImpGamesHandler

--- back-end/src/main/java/edu/brown/cs/student/server/handlers/ImportantGamesHandler.java
+++ back-end/src/main/java/edu/brown/cs/student/server/handlers/ImportantGamesHandler.java
@@ -17,7 +17,6 @@ import spark.Route;
 public final class ImportantGamesHandler implements Route {
   private final Moshi moshi;
   private final Scorer scorer;
-  private final Map<String, Object> responseMap;
 
   /**
    * Constructs a new instance of the ImportantGamesHandler.
@@ -28,7 +27,6 @@ public final class ImportantGamesHandler implements Route {
   public ImportantGamesHandler(Moshi moshi, Scorer scorer) {
     this.moshi = moshi;
     this.scorer = scorer;
-    this.responseMap = new LinkedHashMap<>();
   }
 
   /**
@@ -36,31 +34,30 @@ public final class ImportantGamesHandler implements Route {
    */
   @Override
   public Object handle(Request request, Response response) {
-    this.responseMap.clear();
+    Map<String, Object> responseMap;
     try {
       int gamesCount = Integer.parseInt(request.queryParams("count"));
       List<Event> mostInteresting = scorer.getMostInterestingEvents(gamesCount);
-      this.addEventsToMap(mostInteresting);
+      responseMap = this.addEventsToMap(mostInteresting);
     } catch (NumberFormatException e) {
-      this.responseMap.put("result", "error_bad_request");
+      responseMap = Map.of("result", "error_bad_request");
     }
     return moshi.adapter(Types.newParameterizedType(Map.class, String.class, Object.class))
-        .toJson(this.responseMap);
+        .toJson(responseMap);
   }
 
-  /**
-   * Adds all the events in the list to the responseMap field.
 
-   * @param mostInteresting a list of all the most interesting events
+  /**
+   * Returns all the events in the list in a Map, or puts an error result.
+
+   * @param mostInteresting a List of the most interesting events, a scored by Scorer
+   * @return a Map representing either a successful addition or an error message
    */
-  private void addEventsToMap(List<Event> mostInteresting) {
+  private Map<String, Object> addEventsToMap(List<Event> mostInteresting) {
     if (mostInteresting != null) {
-      this.responseMap.put("result", "success");
-      for (int i = 0; i < mostInteresting.size(); i++) {
-        this.responseMap.put("game" + i, mostInteresting.get(i));
-      }
+      return Map.of("result", "success", "topEvents", mostInteresting);
     } else {
-      this.responseMap.put("result", "error_datasource");
+      return Map.of("result", "error_datasource");
     }
   }
 }
