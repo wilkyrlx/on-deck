586253e4a13c627eb816504fd01183428edf340f
John Wilkinson <93553222+wilkyrlx@users.noreply.github.com>
2023-01-30 16:19:54 -0500

    fix: Access-Control headers in response

--- backend-express/functions/src/index.ts
+++ backend-express/functions/src/index.ts
@@ -1,5 +1,4 @@
 import * as functions from "firebase-functions";
-import { utahJazzMockResponse } from "./data/mockESPNResponse";
 import { slugIDConversionsMap } from "./data/slugIDConversions";
 
 
@@ -16,30 +15,23 @@ export const sports = functions.https.onRequest(async (request, response) => {
     const requestURL: string = API_URL_STUB_SCHED + sportParam + "/" + leagueParam + "/teams/" + teamParamParsed + "/schedule";
     let espnResponse: any;
     // FIXME: incurring some issues with CORS/ deployment. maybe handle promises better?
-    try {
-        const rawResponse: any = await fetch(requestURL);
-        espnResponse = await rawResponse.json();
-        // espnResponse =  utahJazzMockResponse;
-    } catch (error) {
-        response.send({ result: "fetch error", error: error });
-        return;
-    }
 
+    const rawResponse: any = await fetch(requestURL);
+    espnResponse = await rawResponse.json();
 
     const eventList: any[] = [];
-
-    try {
-        espnResponse.events.forEach((event: any) => {
-            const eventDate: Date = new Date(event.date);
-            // TODO: do homeTeamName and awayTeamName the correct way
-            const eventObj: any = { date: eventDate, name: event.name, id: event.id, link: event.links[0].href, homeTeamName: event.competitions[0].competitors[0].team.displayName, awayTeamName: event.competitions[0].competitors[1].team.displayName }
-            eventList.push(eventObj);
-        });
-    } catch (error) {
-        response.send({ result: "foreach error", error: error });
-    }
+    espnResponse.events.forEach((event: any) => {
+        const eventDate: Date = new Date(event.date);
+        // TODO: do homeTeamName and awayTeamName the correct way
+        const eventObj: any = { date: eventDate, name: event.name, id: event.id, link: event.links[0].href, homeTeamName: event.competitions[0].competitors[0].team.displayName, awayTeamName: event.competitions[0].competitors[1].team.displayName }
+        eventList.push(eventObj);
+    });
 
 
     const finalResponse = { result: "success", displayName: espnResponse.team.displayName, eventList: eventList };
+
+    response.setHeader("Access-Control-Allow-Origin", "*")
+    response.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+    response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
     response.send(finalResponse);
 });
